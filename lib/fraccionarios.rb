01703f182e54a4c2eefecf97f8dbae776dedfa5d
alu0100505009 <alu0100505009@ull.edu.es>
2015-10-25 15:10:48 +0000

    Añadidos metodos a la clase

--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -6,9 +6,20 @@ class Fraccionarios
 
 
 	def initialize(num, den)
-		@num, @den = num, den
+		@num_, @den_ = num, den
 	end
 
+	def to_s
+		"#{@num_}/#{@den_}"
+	end
+
+	def to_f 
+        @num_.to_f/@den_.to_f
+   end
+
+	def -@
+        Fraccionarios.new(-@num_,@den_)
+   end
 
 
 
